f6ade11d4ed221d128f869d4065473100c222932
haigbedros <haigbedros@gmail.com>
2018-03-07 17:30:58 -0800

    update server.js static content for the app from the public directory Parse application/json Set Handlebars Import routes and give the server access to them Listener - Start the server

--- server.js
+++ server.js
@@ -5,8 +5,26 @@ var bodyParser = require('body-parser');
 
 // Set up Express App
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Parse application/json
+app.use(bodyParser.json());
+
+// Set Handlebars
+var exphbs = require('express-handlebars');
+
+app.emgine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Import routes and give the server access to them
+var routes = require('./controllers/burgers_controller.js');
+
+app.use(routes);
 
 // Listener - Start the server
 app.listen(PORT, function() {
