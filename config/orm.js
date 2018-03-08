e138c4c6c9669949e51cc96be034284a58077afe
haigbedros <haigbedros@gmail.com>
2018-03-07 16:06:37 -0800

    db and config burger_db creation burgers table creation
    random seeds inserted
    
    config file
    node - mysql connection, exporting connection
    orm file creation and exportation

---
+++ config/orm.js
@@ -0,0 +1,5 @@
+// Import MySQL connection
+var connection = require('../config/connection.js');
+
+// Export ORM
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Cheese Burger', TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Chicken Burger', TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Double Burger', TRUE);
